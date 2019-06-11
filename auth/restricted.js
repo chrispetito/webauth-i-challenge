07a556aa18ecf01a1a9945dac76e5dbb4178a40d
chrispetito <christopherpetito@gmail.com>
2019-06-11 11:30:41 -0700

    update restricted middleware

--- auth/restricted.js
+++ auth/restricted.js
@@ -3,19 +3,9 @@ const bcrypt = require('bcryptjs')
 const users = ('../users/users-model.js')
 
 module.exports = function restricted(req, res, next) {
-    const { username, password } = req.headers;
-
-    if(username && password) {
-        users.findBy({ username }).first().then(user => {
-            if (user && bcrypt.compareSync(password, user.password)) {
-                next()
-            } else {
-                res.status(401).json({ message: 'You shall not pass!'})
-            }
-        }).catch(err => {
-            res.status(500).json(err)
-        })
-    } else {
-        res.status(400).json({ message: 'Please provide credentials'})
-    }
+   if (req.session && req.session.username) {
+       next()
+   } else {
+       res.status(401).json({ message: 'You shall not pass!'})
+   }
 }
